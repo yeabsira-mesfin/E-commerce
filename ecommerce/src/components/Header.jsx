43b03a424ae21684e0a4506d2be0aa26cbcc5794
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-05-21 06:01:47 -0400

    margin for navigation bar fixed

--- ecommerce/src/components/Header.jsx
+++ ecommerce/src/components/Header.jsx
@@ -5,14 +5,13 @@ import {Menu, X} from 'react-feather';
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-
   return (
     <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-        <h1 className="text-2xl">E-commerce Site</h1>
+        <h1 className="text-2xl mb-20 md:mb-0">E-commerce Site</h1>
         <nav>
             <div className="md:hidden">
                 <button onClick={()=> setIsOpen(!isOpen)}>
-                    {isOpen? <X/>: <Menu/>}
+                    {isOpen? <X className='ml-5'/>: <Menu className='mb-20 md:mb-0'/>}
                 </button>
             </div>
             <ul className={`md:flex ${isOpen ? 'block': 'hidden'}`}>
