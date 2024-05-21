cc9fb0ac7a5e707055c86705253e24e15acef0e2
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-05-21 06:30:00 -0400

    Cart page

--- ecommerce/src/pages/CartPage.jsx
+++ ecommerce/src/pages/CartPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Cart from '../components/Cart'
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <div>
+      <h2 className='text-3xl font-bold my-4'>Your Cart</h2>
+      <Cart/>
+    </div>
   )
 }
 
