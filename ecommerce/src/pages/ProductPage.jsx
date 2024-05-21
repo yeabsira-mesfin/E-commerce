88d17c52ebe980d47ef351999eb63aa97bdb452b
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-05-21 06:28:18 -0400

    prduct page

--- ecommerce/src/pages/ProductPage.jsx
+++ ecommerce/src/pages/ProductPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import ProductDetail from '../components/ProductDetail'
 
 const ProductPage = () => {
-  return (
-    <div>ProductPage</div>
-  )
+  const {productId} = useParams();
+  return <ProductDetail productId= {productId}/>
 }
 
 export default ProductPage
