f704a79a3fc3b257f1f8678fee09e50a4b607580
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-05-21 06:58:33 -0400

    cart slice

---
+++ ecommerce/src/redux/slices/cartSlice.js
@@ -0,0 +1,20 @@
+// src/redux/slices/cartSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItemToCart: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeItemFromCart: (state, action) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addItemToCart, removeItemFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
